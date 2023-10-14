7cf541c876c26c63c74078106e1727d7481b9bc0
Dmitry Grishin <kenpxrkasf@gmail.com>
2023-10-14 15:55:23 +0300

    commit number 2

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 // алгоритм бинарного поиска. 
 let mas = [1,2,3,4,5,14,7,8,9,10];
 function BinarySearch(arr, value){
+    //границы поиска
     let start = -1;
     let end = arr.length;
-    
+    //цикл
     while(end - start > 1){
         const mid = Math.floor((start+end)/2);
         if(value === arr[mid]){
-            return mid;
+            //возвращаем индекс нужного элемента.
+            return mid; 
         }
         else if(value < arr[mid]){
             end = mid;
@@ -20,3 +22,11 @@ function BinarySearch(arr, value){
     return -1;
 } 
 console.log(BinarySearch(mas, 2));
+//
+//
+//
+//
+//
+//
+//
+//
--- test2.js
+++ test2.js
@@ -1,1 +1,2 @@
 console.log("Hello World");
+console.log("I made my first commit!");
